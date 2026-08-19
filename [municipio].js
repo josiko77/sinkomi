cf5ca9b04a66375d719fd27db6a7aadb5ba1411b
josiko77 <aquiles20006@gmail.com>
2026-08-19 08:09:54 +0200

    Add files via upload

--- [municipio].js
+++ [municipio].js
@@ -1,6 +1,6 @@
-// api/comprar/[municipio].js
+// api/alquiler/[municipio].js
 const { generateMunicipioPage } = require('../_municipios-helper');
 
 module.exports = async (req, res) => {
-  await generateMunicipioPage(req, res, 'comprar');
+  await generateMunicipioPage(req, res, 'alquiler');
 };
